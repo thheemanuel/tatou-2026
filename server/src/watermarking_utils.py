33c1fb4d5c880d3792ec957096d99bbc9ca6871f
Gustav Dahlberg <gustav.dahlberg951@gmail.com>
2026-09-21 08:51:35 +0200

    Unregister invisible-text watermarking method until it has key auth

--- server/src/watermarking_utils.py
+++ server/src/watermarking_utils.py
@@ -42,14 +42,12 @@ from watermarking_method import (
     load_pdf_bytes,
 )
 from add_after_eof import AddAfterEOF
-from watermarking_method_gustav import InvisibleTextWatermark
 # --------------------
 # Method registry
 # --------------------
 
 METHODS: Dict[str, WatermarkingMethod] = {
     AddAfterEOF.name: AddAfterEOF(),
-    InvisibleTextWatermark.name: InvisibleTextWatermark(),
 }
 """Registry of available watermarking methods.
 
